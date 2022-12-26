61d611fd4d6dd773d0d6b9feed3252b360116728
UBA-code <belhachmiyassine4@gmail.com>
2022-12-26 01:16:01 +0100

    stop at develop moving top algo at line 97 in sort_hundred file

--- sort_hundred.c
+++ sort_hundred.c
@@ -6,12 +6,12 @@
 /*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:15:28 by ybel-hac          #+#    #+#             */
-/*   Updated: 2022/12/25 01:52:05 by ybel-hac         ###   ########.fr       */
+/*   Updated: 2022/12/26 01:11:21 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-/*
+
 int get_size(t_stack *stack)
 {
 	int i;
@@ -49,7 +49,7 @@ int get_position(t_stack *stack, int num)
 	return (0);
 }
 
-void	stack_to_arr(t_stack *stack, int *tab)
+void stack_to_arr(t_stack *stack, int *tab)
 {
 	int i;
 
@@ -63,7 +63,7 @@ void	stack_to_arr(t_stack *stack, int *tab)
 	tab[i] = stack->num;
 }
 
-void	sort(int *tab, t_stack *stack, int size)
+void sort(int *tab, t_stack *stack, int size)
 {
 	int i;
 	int x;
@@ -90,13 +90,15 @@ void chunk_work(chunks_utils *utils, int min, int max)
 
 	i = 0;
 	current = *(utils->stack_a);
-	while (current->next)
+	while (current)
 	{
 		if (current->num >= min && current->num <= max)
 		{
-			
-		}
+			move_top(*utils, utils->stack_a, current->num);
 			push_top_b(utils->stack_a, utils->stack_b);
+			current = *(utils->stack_a);
+			continue;
+		}
 		current = current->next;
 	}
 }
@@ -106,11 +108,18 @@ void chunks(chunks_utils *utils, int *tab)
 	int min;
 	int max;
 	int chunk_size_temp;
-	int i;
+	static int last;
 
-	chunk_size_temp = utils.chunk_size;
+	chunk_size_temp = utils->chunks_size;
 	// here maybe sigfault will be get because the chunks size not in all time fixed
-	max = tab[utils->chunks_size];
+	max = 0;
+	while (last < utils->size && chunk_size_temp > 0)
+	{
+		last++;
+		max++;
+		chunk_size_temp--;
+	}
+	max = tab[max - 1];
 	min = tab[0];
 	chunk_work(utils, min, max);
 }
@@ -119,17 +128,16 @@ void sort_hundred(t_stack **stack_a, t_stack **stack_b)
 {
 	chunks_utils utils;
 
-	utils.size = get_size(stack_a);
+	utils.size = get_size(*stack_a);
 	utils.chunks_size = utils.size / 10;
 	utils.i = 0;
 	utils.tab = malloc(sizeof(int) * utils.size);
 	utils.stack_a = stack_a;
 	utils.stack_b = stack_b;
-	sort(&(utils.tab), *stack_a, utils.size);
+	sort(utils.tab, *stack_a, utils.size);
 	while (*stack_a)
 	{
 		chunks(&utils, &(utils.tab[utils.i]));
-		utils.i += utils.chunks_size
+		utils.i += utils.chunks_size;
 	}
 }
-*/
